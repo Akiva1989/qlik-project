dc640ab5589e752266992309aeb59b2e2904dc60
akiva adam <akiva@eyecu.digital>
2022-03-04 11:50:52 +0200

    add enviromental variables

--- src/postgres/postgres.js
+++ src/postgres/postgres.js
@@ -1,16 +1,20 @@
-const Sequelize = require('sequelize')
+const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize("drp20gnt8jf0i", "jfrgcpvrwreeej", "62eb841ae0fd8e52d0568240a3723357204fa0ea970cc6b771f87996b30351f6", {
-    host: "ec2-52-70-186-184.compute-1.amazonaws.com",
-    port: 5432,
-    logging: false,
-    dialect: "postgres",
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-     },
-})
+const sequelize = new Sequelize(
+    process.env.POSTGRES_DB_NAME, 
+    process.env.POSTGRES_USER_NAME, 
+    process.env.POSTGRES_PASSWORD, 
+    {
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        logging: false,
+        dialect: "postgres",
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        },
+});
 
 module.exports = sequelize;
