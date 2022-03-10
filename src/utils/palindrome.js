6bd2c2a45419a279a489b99276e72be3f350d607
akiva adam <akiva@eyecu.digital>
2022-03-10 09:18:23 +0200

    change

--- src/utils/palindrome.js
+++ src/utils/palindrome.js
@@ -1,5 +1,7 @@
 const isPalindrome = (message) => {
+    // remove all special characters from the string
     const clean = message.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+    //check if the word is the same when it is reversed
     return (clean.length > 0) && Array.from(clean).reverse().join('') === clean;
 };
 
